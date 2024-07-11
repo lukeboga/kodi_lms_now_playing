a551cdb44d2f7196e134bb4a2b95d88c7058b32f
LukeBM <lukebm@duck.com>
2024-07-11 08:31:00 +0100

    Add log_message.py: Utility function for logging messages

---
+++ src/resources/lib/utils/log_message.py
@@ -0,0 +1,16 @@
+import xbmc
+
+def log_message(message, level=xbmc.LOGINFO):
+    """
+    Log a message to the Kodi log with the specified log level.
+    
+    Parameters:
+        message (str): The message to log.
+        level (int): The log level for the message. Default is xbmc.LOGINFO.
+    """
+    try:
+        xbmc.log(message, level=level)
+    except Exception as e:
+        # Log any errors encountered during logging
+        xbmc.log(f"KLMS Addon: Error logging message: {e}", level=xbmc.LOGERROR)
+
