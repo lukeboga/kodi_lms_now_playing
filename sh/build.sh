dee15ff58de480a81cfb2884eb2dd38abb2537c8
LukeBM <lukebm@duck.com>
2024-07-12 08:51:01 +0100

    Update build script

--- sh/build.sh
+++ sh/build.sh
@@ -5,11 +5,11 @@ root_dir="$home_dir/Development/Kodi/kodi_lms_now_playing"
 src_dir="$root_dir/src"
 dest_dir="$root_dir/dest"
 version="v0.0.1"
-timestamp=$(date +"%Y-%m-%d-%H%M%S")
+timestamp=$(date +"%Y-%m-%d-%H.%M.%S")
 addon_name="klms-addon-$version-$timestamp"
 temp_build_dir="$dest_dir/$addon_name"
 zip_name="$addon_name.zip"
-kodi_dir="$home_dir/Music"
+kodi_dir="$home_dir/_Tmp"
 
 echo "Building zip file for Kodi..."
 mkdir $temp_build_dir
